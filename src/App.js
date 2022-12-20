3c400aa5ef6d7772c663cbe505c513edbb530c23
Koreant_051 <dg.bomikang@gmail.com>
2022-12-20 23:39:41 +0900

    다양한사용자 입력 및 배열 리스트 처리하기 (#1)
    - [x] 한 줄 입력 처리하기
    - [x] 여러 줄 입력 처리하기
    - [x] 선택 박스 입력 처리하기
    - [x] 사용자 입력 데이터 핸들링 하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import DiaryEditor from "./DiaryEditor";
 
 function App() {
   return (
     <div className="App">
-      <h2>test</h2>
+      <DiaryEditor />
     </div>
   );
 }
