8b0d45846d6decb3a898b9a401a70fab7cea6191
Koreant_051 <dg.bomikang@gmail.com>
2022-12-25 02:20:57 +0900

    feat: 일기 아이템 수정 기능 구현

--- src/DiartItem.js
+++ src/DiartItem.js
@@ -1,31 +1,75 @@
+import { useRef, useState } from "react";
+
 const DiaryItem = ({
   id,
   author,
   content,
   emotion,
   created_date,
-  onDelete
+  onRemove,
+  onEdit
 }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const [localContent, setLocalContent] = useState(content);
+  const localContentInput = useRef();
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handleRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+
+    if (window.confirm(`${id}번째 일기를 수정하시겠습니까?`))
+      onEdit(id, localContent);
+    toggleIsEdit();
+  };
+
   return (
     <div className="DiaryItem">
       <span className="info">
         작성자: {author} | 감정점수: {emotion}
-        <button
-          onClick={() => {
-            console.log(id);
-            if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
-              onDelete(id);
-            }
-          }}
-        >
-          삭제하기
-        </button>
+        {isEdit ? (
+          <>
+            <button onClick={handleQuitEdit}>수정취소</button>
+            <button onClick={handleEdit}>수정완료</button>
+          </>
+        ) : (
+          <>
+            <button onClick={toggleIsEdit}>수정하기</button>
+            <button onClick={handleRemove}>삭제하기</button>
+          </>
+        )}
       </span>
 
       <br />
       <span className="date">{new Date(created_date).toLocaleString()}</span>
       <br />
-      <span className="content">{content}</span>
+      <span className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </span>
     </div>
   );
 };
